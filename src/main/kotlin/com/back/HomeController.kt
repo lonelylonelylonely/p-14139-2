6904f05fcc6e0d77a08bc5497fbb69bbba2f40d9
lonelylonelylonely <lonelylonelylonely@example.com>
2026-03-05 14:17:26 +0900

    5

--- src/main/kotlin/com/back/HomeController.kt
+++ src/main/kotlin/com/back/HomeController.kt
@@ -11,7 +11,7 @@ class HomeController(
 ) {
     @GetMapping("/")
     fun main(): String {
-        return "secretWord!! : $secretWord"
+        return "secretWord__ : $secretWord"
     }
 
     @GetMapping("/buckets")
